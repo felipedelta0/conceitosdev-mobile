ef15c75daeef3c37cda0937a75bb2ecbea641958
Luis Felipe <felipedelta0@gmail.com>
2020-08-02 23:55:35 -0300

    Listando projetos da API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import { View, Text, StatusBar, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, FlatList, Text, StatusBar, StyleSheet } from 'react-native'
+
+import api from './services/api'
 
 export default function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
   return (
     <>
       <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.title}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   )
 }
@@ -15,14 +32,12 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#7159c1',
-		justifyContent: 'center',
-		alignItems: 'center'
+		backgroundColor: '#7159c1'
 	},
 	
 	title: {
 		color: '#fff',
-		fontSize: 32,
+		fontSize: 20,
 		fontWeight: 'bold'
 	}
 })
